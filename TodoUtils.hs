c5f41baf87762654c20dc574a8f0def110265e28
Martino Visintin <mvisinti@thoughtworks.com>
2014-05-12 17:04:31 +0100

    add priorities to todos

--- TodoUtils.hs
+++ TodoUtils.hs
@@ -7,7 +7,8 @@ module TodoUtils
 , completeTodo
 ) where
 
-import Data.List (partition)
+import Data.List (partition, sortBy)
+import Data.Char (toUpper)
 import qualified Data.Text as Text
 
 data Todo = Todo Int String deriving (Eq)
@@ -15,6 +16,22 @@ data Todo = Todo Int String deriving (Eq)
 instance Show Todo where
   show (Todo todoId todoText) = show todoId ++ " " ++ todoText
 
+instance Ord Todo where
+  compare t1 t2 = compare (priority t1) (priority t2)
+
+type Priority = Maybe Char
+priority :: Todo -> Priority
+priority (Todo _ todoText) = -- FIXME: use regexpes...
+  case priorityChar todoText of
+    Just pri -> if pri `elem` ['A'..'E']
+                  then Just (toUpper pri)
+                  else Nothing
+    Nothing -> Nothing
+  where
+    priorityChar :: String -> Maybe Char
+    priorityChar ('(':pri:')':_) = Just pri -- incomplete todo with priority
+    priorityChar ('x':' ':'(':pri:')':_) = Just pri -- complete todo with priority
+    priorityChar _ = Nothing
 
 readTodoTxt :: String -> [Todo]
 readTodoTxt todoTxt =
@@ -23,7 +40,7 @@ readTodoTxt todoTxt =
         blank = ([]==) . Text.unpack . Text.strip . Text.pack
 
 displayTodoTxt :: [Todo] -> String
-displayTodoTxt = unlines . map show
+displayTodoTxt = unlines . map show . sortBy (flip compare)
 
 serialiseTodoTxt :: [Todo] -> String
 serialiseTodoTxt = unlines . map (\(Todo _ text) -> text)
@@ -49,4 +66,3 @@ completeTodo targetTodoId todoList =
                                                      | otherwise      = Todo tId ("x " ++ tText)
                                                  completed (Todo _ ('x':' ':_)) = True
                                                  completed (Todo _ _) = False
-
