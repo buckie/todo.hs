dac796e6c3d2b155487e36a48ac620936dd91dde
Martino Visintin <mvisinti@thoughtworks.com>
2014-07-08 13:44:38 +0200

    add fixmes

--- FilePaths.hs
+++ FilePaths.hs
@@ -6,12 +6,16 @@ module FilePaths
 import System.Directory
 import System.Environment (getEnv)
 
+-- FIXME: should be localTodoTxtPath
 localTodoFilePath :: String
 localTodoFilePath = "./todo.txt"
 
+-- FIXME: should be localDoneTxtPath
 localArchiveFilePath :: String
 localArchiveFilePath = "./done.txt"
 
+-- FIXME: use a dotfile for these
+-- TODO: create a dotfile on first run
 getTodoTxtFilePath :: IO FilePath
 getTodoTxtFilePath = do
   localTodoFile <- doesFileExist localTodoFilePath
